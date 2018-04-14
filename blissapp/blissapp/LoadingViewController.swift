a401f8d5edd148b39fa27b7627af6cd4570f09b8
Luis Carlos Rosa <lcbr01@gmail.com>
2018-04-14 16:21:38 +0100

    LoadingViewController makes a check for the network status

---
+++ blissapp/blissapp/LoadingViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  blissapp
+//
+//  Created by Luis Carlos Rosa on 14/04/18.
+//  Copyright © 2018 Luis. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var retryButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        getHealth()        
+    }
+    
+    func getHealth() {
+        let networkManager = NetworkController()
+        
+        networkManager.getHealth { (data, error) in
+            guard let data = data else {
+                self.showRetry()
+                return
+            }
+            do {
+                if let statusDict = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                    guard let status = statusDict["status"] as? String else {
+                        self.showRetry()
+                        return
+                    }
+                    if status == "OK" {
+                        // go to next Screen
+                        print("go to next Screen")
+                    } else {
+                        self.showRetry()
+                    }
+                }
+            }
+            catch {
+                self.showRetry()
+            }
+        }
+    }
+    
+    func showRetry() {
+        retryButton.setTitle("Retry", for: .normal)
+        retryButton.isEnabled = true
+        activityIndicator.stopAnimating()
+    }
+    
+    @IBAction func retryAction(_ sender: Any) {
+        retryButton.setTitle("Loading", for: .normal)
+        retryButton.isEnabled = false
+        activityIndicator.startAnimating()
+        getHealth()
+    }
+    
+}
+
